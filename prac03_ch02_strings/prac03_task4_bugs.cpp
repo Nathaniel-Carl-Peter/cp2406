b535f830baeb6e924698e71397ab85f467392191
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-06-21 14:23:51 +1000

    prac 03 completed task 1 completed

--- prac03_ch02_strings/prac03_task4_bugs.cpp
+++ prac03_ch02_strings/prac03_task4_bugs.cpp
@@ -10,27 +10,29 @@ using namespace std;
 int main()
 {
 
-	string userInput;
-	getline(cin, userInput);
-	cout << "User Input: ";
+	// string userInput;
+	// getline(cin, userInput);
+	// cout << "User Input: ";
 
 	// A vector to store all values.
 	vector<double> values;
 
 	// Ask the user to enter values until 0 is entered.
 	cout << "Input values, one at a time followed by Enter:" << endl;
-	while (true); {;
+	while (true)
+	{
 		double value;
 		cin >> value;
-		if (value = 0.0); {
-			
+		if (value == 0.0)
+		{
+			break;
 		};
 		values.push_back(value);
 	};
 
 	// Format the values in a table with a couple of columns.
-	for (auto value : values); {
-		cout << "{0:16e} | {0:12f} | {0:<12g} | {0:#>+12g}" << values[0] << endl;
+	for (auto value : values) {
+		cout << value << endl;
 	}
 
 }
